278b35815c34f959e854f7ad5e1fc3d7c345a333
Rocik <rrocik@vp.pl>
2017-05-21 11:02:06 +0200

    Save the best population's result

--- app.cpp
+++ app.cpp
@@ -24,6 +24,9 @@ void App::run() {
 
 	population.fitness(input);
 
+	unsigned long long bestFitness = ULONG_MAX;
+	Mat bestImage;
+	
 	int i = 0;
 	for(; i < populations; ++i) {
 		population.selectionRoulette();
@@ -32,25 +35,33 @@ void App::run() {
 		
 		population.fitness(input);
 
-		drawImages(input, population.topResult(), i, population.topFitness());
+		unsigned long long currentFitness = population.topFitness();
+		if (currentFitness < bestFitness) {
+			bestFitness = currentFitness;
+			bestImage = population.topResult();
+		}
+		
+		drawImages(input, bestImage, population.topResult(), i, bestFitness);
 
 		// Stop when key is pressed
 		if (waitKey(1) == 'q')
 			break;
 	}
 	
-	drawImages(input, population.topResult(), i, population.topFitness());
+	drawImages(input, bestImage, population.topResult(), i, population.topFitness());
 	waitKey(0);
 }
 
 
-void App::drawImages(Mat image1, Mat image2, int pid, unsigned long long fitness) {
-	Mat dst = Mat(image1.rows, image1.cols * 2, CV_8UC3, Scalar(0, 0, 0));
+void App::drawImages(Mat image1, Mat image2, Mat image3, int pid, unsigned long long fitness) {
+	Mat dst = Mat(image1.rows, image1.cols * 3, CV_8UC3, Scalar(0, 0, 0));
 
 	Mat output = dst(Rect(0, 0, image1.cols, image1.rows));
 	image1.copyTo(output);
 	output = dst(Rect(image1.cols, 0, image1.cols, image1.rows));
 	image2.copyTo(output);
+	output = dst(Rect(image1.cols * 2, 0, image1.cols, image1.rows));
+	image3.copyTo(output);
 
 	sprintf(buffer, "P: %d", pid + 1);
 	putText(dst, buffer, pPos, FONT_HERSHEY_PLAIN, 1.0, white, 1, CV_AA);
