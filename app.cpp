18e8ea6dab6e6fe561f50eaed46d91d06fcd7496
Rocik <rrocik@vp.pl>
2017-05-25 21:28:03 +0200

    Change cat

--- app.cpp
+++ app.cpp
@@ -22,12 +22,11 @@ void App::run() {
 	Mat input = imread((const char*[]) { "",
 		"BitwaPodGrunwaldem.jpg", // 1
 		"cat.jpg",		          // 2
-		"mona.jpg",		          // 3
-		"spongebob.jpg",          // 4
-		"saitama.jpg",            // 5
-		"scream.jpg",             // 6
-        "result.svg.png"          // 7
-	}[ 6 ]);
+		"spongebob.jpg",          // 3
+		"saitama.jpg",            // 4
+		"scream.jpg",             // 5
+        "result.svg.png"          // 6
+	}[ 2 ]);
 	
 	int newWidth = input.cols - (input.cols % 4);
 	resize(input, input, Size(newWidth, input.rows), 0, 0, INTER_CUBIC);
